01d4b8dd414d95005ba3c1ede6a4ef126b38c7d0
gaberabreu <gaberabreu.github@gmail.com>
2024-11-08 23:32:44 -0300

    Add Vitest

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 const App = () => {
-  return <div>App</div>;
+  return <div>App.tsx</div>;
 };
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest/config" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
@@ -15,6 +17,15 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    css: true,
+    coverage: {
+      reporter: ["html", "lcov"],
     },
   },
 });
