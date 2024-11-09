20cfa151f46d2978b6c2c7a56e0c5da240233ba4
gaberabreu <gaberabreu.github@gmail.com>
2024-11-08 23:30:17 -0300

    Add Alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+import path from "path";
+
 export default defineConfig({
   plugins: [react()],
   css: {
@@ -8,6 +10,11 @@ export default defineConfig({
       scss: {
         api: "modern-compiler",
       },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
