8c8c831f1d2430c97e941ff96a57424f595515d8
gaberabreu <gaberabreu.github@gmail.com>
2024-11-09 11:45:28 -0300

    Add MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
 const App = () => {
-  return <div>App.tsx</div>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography
+          variant="h4"
+          component="h1"
+          sx={{ mb: 2 }}
+        >
+          App.tsx {count}
+        </Typography>
+        <Button
+          variant="contained"
+          onClick={() => setCount((prev) => prev + 1)}
+        >
+          Click me
+        </Button>
+      </Box>
+    </Container>
+  );
 };
 
 export default App;
